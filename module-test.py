04dff07ef5b6a0e57bb00619820272b861667e00
csw <chenshihwei@pm.me>
2021-12-11 20:33:54 +0800

    feat: test module

--- module-test.py
+++ module-test.py
@@ -1,7 +1,11 @@
 import random
 import names
+import tool
 
 
 # print(random.gammavariate(0.01, 1.23))
 
 print(names.get_full_name())
+
+print(tool.AddMe(100, 200))
+print(tool.AddMe(names.get_first_name(), names.get_last_name()))
---
+++ tool.py
@@ -0,0 +1,8 @@
+
+def AddMe(number1, number2):
+    if (isinstance(number1, int) & isinstance(number2, int)):
+        return number1 + number2
+    else:
+        return number1 + " " + number2
+# pName = "tom"
+# print(type(pName))
