023096d7ac5315a43e5931b8e621fad49d54a61a
csw <chenshihwei@pm.me>
2021-12-12 19:12:48 +0800

    chore: play string

--- string-test.py
+++ string-test.py
@@ -9,3 +9,13 @@ hello,
 
 print(s1, s2, s3, end='')
 # print(s1, s2, s3)
+
+s11 = "hello " * 3
+s12 = "world"
+s11 += s12
+print(s11)
+
+print("ll" in s11)
+print("test" in s11)
+print(s11.capitalize())
+print(s11.title())
