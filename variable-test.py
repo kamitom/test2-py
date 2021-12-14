138c9e9fe27090998bf1c182b18bbedce8eb8227
csw <chenshihwei@pm.me>
2021-12-14 20:37:36 +0800

    chore: variable naming test

---
+++ variable-test.py
@@ -0,0 +1,28 @@
+print(3**2)
+
+print(9 % 2)  # 取 餘數
+
+print(7 // 2)
+
+print(-7 // 2)
+
+print((23+32+64)/3)
+
+height, weight = 178, 61
+
+print('height is', height)
+print('weight is', weight)
+
+
+# 命名变量的方式称之为 snake case，因为我们用下划线连接单词
+# mv_population = 74728
+# mv_area = 11.995
+# mv_population, mv_area = 74728, 11.995
+
+mv_population = 74728
+mv_population += 4000-600
+mv_area = 11.995
+
+mv_density = mv_population / mv_area
+print(mv_population)
+print(mv_density)
