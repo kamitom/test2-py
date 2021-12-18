f1c4b32c193aadaedc245b45393b7eb87f9f4f6b
csw <chenshihwei@pm.me>
2021-12-18 23:46:27 +0800

    chore: list slicing

---
+++ list.udacity.py
@@ -0,0 +1,13 @@
+lst_of_random_things = [1, 3.4, 'a string', True]
+months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN',
+          'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
+
+print(len(lst_of_random_things))
+print(lst_of_random_things[-2])
+
+# slicing
+print(months[6:9])
+print(months[6:])
+print(months[:9])
+
+print(lst_of_random_things[1:2])
