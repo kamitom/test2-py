21185bba1b8f7f0f9e121df41fb86f076c1d9371
Rfd Me <tom.chen@rfdme.com>
2021-12-20 19:03:26 +0800

    chore: band name generator

---
+++ band-name-generator-end.py
@@ -0,0 +1,5 @@
+
+print('Welcome to the Band Name Generator.')
+city_name = str(input('What\'s name of the city you grew up in?\n'))
+pet_name = str(input('What\'s your pet\'s name?\n'))
+print('Your band name could be {} {}'.format(city_name, pet_name))
