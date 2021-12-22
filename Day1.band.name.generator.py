37a37cc3ea0fec08921cae086c1cd38e2c51eb3d
csw <chenshihwei@pm.me>
2021-12-22 19:24:44 +0800

    chore: Day 1

---
+++ Day1.band.name.generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the band generator.")
+band_name = str(input("What\'s name the city you grew in?\n"))
+pet_name = str(input("What\'s your pet\'s name?\n"))
+print("Your band name could be {} {}".format(
+    band_name.title(), pet_name.title()))
---
+++ Day1.py
@@ -0,0 +1,24 @@
+
+print(('hello world!\n'*3).title())
+
+# name = str(input('what is your name?\n'))
+# print('hello, {}'.format(name))
+
+print('hello, ' + input('what is her name?\n'))
+
+print(len(input('What is your name? ')))
+
+# switch a , b
+
+a = input('a = ')
+b = input('b = ')
+
+# a1 = b
+# b1 = a
+c = a
+a = b
+b = c
+
+
+print('a = ', a)
+print('b = ', b)
