8314eee604a646f9b9ac24bdd4854740da857572
csw <chenshihwei@pm.me>
2022-01-05 12:46:55 +0800

    chore: two digits add them both

--- Day2.py
+++ Day2.py
@@ -1,6 +1,9 @@
 # Data Types
 
 # String
+from io import TextIOWrapper
+
+
 print("Hello"[4])
 
 # Int
@@ -10,4 +13,13 @@ print(123_456_786)
 print(123_456.789)
 
 print(len("123_456"))
-print(len(100))
+# print(len(100))
+
+vlan_ids = 2**12
+print('vlan id, ', vlan_ids)
+
+two_digit_number = input('Type a two digit number: ')
+first_number = two_digit_number[0]
+second_number = two_digit_number[1]
+
+print('result ', int(first_number)*int(second_number))
